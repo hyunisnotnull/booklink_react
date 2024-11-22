22d8f45d281e3453252a403cf28dcefca1a35dad
hyun <bongtacker@naver.com>
2024-11-22 17:40:01 +0900

    edit search_book

--- src/comp/Navbar.jsx
+++ src/comp/Navbar.jsx
@@ -7,13 +7,13 @@ const Navbar = () => {
     <nav className="navbar">
       <ul className="nav-links">
         <li className="nav-item">
-          <Link to="/search">도서 상세검색</Link>
+          <Link to="/search_book">도서 상세검색</Link>
         </li>
         <li className="nav-item">
-          <Link to="/library">도서관 찾기</Link>
+          <Link to="/search_library">도서관 찾기</Link>
         </li>
         <li className="nav-item">
-          <Link to="/my-library">내 서재</Link>
+          <Link to="/my_library">내 서재</Link>
         </li>
       </ul>
     </nav>
